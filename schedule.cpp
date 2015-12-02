9db400ca6a8495b12776886c7c6c148532327842
Ben Holden <bholden1@andrew.cmu.edu>
2015-12-02 18:23:30 -0500

    QT tests/learning

--- schedule.cpp
+++ schedule.cpp
@@ -4,6 +4,8 @@
 #include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QAction>
+#include <QPushButton>
+#include <QApplication>
 
 schedule::schedule()
 {
@@ -14,9 +16,38 @@ schedule::schedule()
     a->setText( "Quit" );
     connect(a, SIGNAL(triggered()), SLOT(close()) );
     menuBar()->addMenu( "File" )->addAction( a );
+    
+    // Set size of the window
+    setFixedSize(500, 250);
+    
+//     QPushButton *m_button;
+    // Create and position the button
+    m_button = new QPushButton("Hello World", this);
+    m_button->setGeometry(10, 10, 80, 30);
+    m_button->setCheckable(true);
+    
+    // Set the counter to 0
+    m_counter = 0;
+    
+    connect(m_button, SIGNAL (clicked(bool)), this, SLOT (slotButtonClicked(bool)));
+    connect(this, SIGNAL (counterReached()), QApplication::instance(), SLOT (quit()));
 }
 
 schedule::~schedule()
 {}
+
+void schedule::slotButtonClicked(bool checked)
+{
+    if (checked) {
+        m_button->setText("Checked");
+    } else {
+        m_button->setText("Hello World");
+    }
+    
+    m_counter ++;
+    if (m_counter == 10) {
+        emit counterReached();
+    }
+}
 
 #include "schedule.moc"
--- schedule.h
+++ schedule.h
@@ -2,6 +2,8 @@
 #define schedule_H
 
 #include <QtGui/QMainWindow>
+#include <QPushButton>
+#include <QApplication>
 
 class schedule : public QMainWindow
 {
@@ -9,6 +11,13 @@ Q_OBJECT
 public:
     schedule();
     virtual ~schedule();
+signals:
+    void counterReached();
+private:
+    void slotButtonClicked(bool checked);
+private:
+    int m_counter;
+    QPushButton *m_button;
 };
 
 #endif // schedule_H
